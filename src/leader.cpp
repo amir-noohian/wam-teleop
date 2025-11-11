6506eb210fe8bb332fd6403e142f25936f8fc6d4
amir-noohian <amirhossein76.noohian@gmail.com>
2025-11-11 15:24:21 -0700

    added wrist to leader

--- src/leader.cpp
+++ src/leader.cpp
@@ -1,8 +1,10 @@
+/*
+ * leader.cpp (wrist-enabled)
+ */
+#include "external_torque.h"
 #include <iostream>
 #include <string>
-
 #include <boost/thread.hpp>
-
 #include <barrett/detail/stl_utils.h>
 #include <barrett/os.h>
 #include <barrett/products/product_manager.h>
@@ -10,193 +12,217 @@
 #include <barrett/units.h>
 
 #define BARRETT_SMF_VALIDATE_ARGS
-#include "ros/ros.h"
 #include <barrett/standard_main_function.h>
+#include "ros/ros.h"
 
+#include <haptic_wrist/haptic_wrist.h>
+
+#include "leader.h"                     // wrist-enabled header above
 #include "background_state_publisher.h"
-#include "leader.h"
-#include "tool_frame_cb.h"
-#include "external_torque.h"
+#include "leader_dynamics.h"
+#include "dynamic_external_torque.h"
 
 using namespace barrett;
 using detail::waitForEnter;
 
 void printUsage(const std::string& programName, const std::string& remoteHost, int recPort, int sendPort) {
-    std::cout << "Usage: " << programName << " [remoteHost] [recPort] [sendPort]" << std::endl;
-    std::cout << "       Defaults: remoteHost=" << remoteHost << ", recPort=" << recPort << ", sendPort=" << sendPort
-              << std::endl;
-    std::cout << "       -h or --help: Display this help message." << std::endl;
+    std::cout << "Usage: " << programName << " [remoteHost] [recPort] [sendPort]\n";
+    std::cout << "       Defaults: remoteHost=" << remoteHost << ", recPort=" << recPort << ", sendPort=" << sendPort << "\n";
+    std::cout << "       -h or --help: Display this help message.\n";
 }
 
 bool validate_args(int argc, char** argv) {
-
     if ((argc == 2 && (std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help")) || (argc > 4)) {
         printUsage(argv[0], "127.0.0.1", 5555, 5554);
         return 0;
     }
-
     return true;
 }
+
 template <size_t DOF>
-int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) {
+int wam_main(int argc, char **argv, ProductManager &pm, systems::Wam<DOF> &wam) {
     BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
 
-    jp_type SYNC_POS; // the position each WAM should move to before linking
+    jp_type SYNC_POS;
     if (DOF == 4) {
         SYNC_POS[0] = 0.0;
-        SYNC_POS[1] = -1.5;
+        SYNC_POS[1] = -1.95;
         SYNC_POS[2] = 0.0;
-        SYNC_POS[3] = 2.7;
-
+        SYNC_POS[3] = 2.97;
     } else {
-        printf("Error: Only 4 DOF wam supported\n");
+        printf("Error: 4 DOF supported\n");
         return false;
     }
+
     std::string remoteHost = "127.0.0.1";
-    int rec_port = 5555;
-    int send_port = 5554;
+    int rec_port = 5555;  // leader receives follower's 5555
+    int send_port = 5554; // leader sends on 5554
 
-    if (argc >= 2) {
-        remoteHost = std::string(argv[1]);
-    }
-    if (argc >= 3) {
-        rec_port = std::atoi(argv[2]);
-    }
-    if (argc >= 4) {
-        send_port = std::atoi(argv[3]);
-    }
+    if (argc >= 2) remoteHost = std::string(argv[1]);
+    if (argc >= 3) rec_port   = std::atoi(argv[2]);
+    if (argc >= 4) send_port  = std::atoi(argv[3]);
 
+    ros::init(argc, argv, "leader");
     haptic_wrist::HapticWrist hw;
     hw.gravityCompensate(true);
     hw.run();
 
-    ros::init(argc, argv, "leader");
     BackgroundStatePublisher<DOF> state_publisher(pm.getExecutionManager(), wam, &hw);
 
-    ToolFrameCb toolframeCb(&hw);
-    systems::connect(wam.toolPose.output, toolframeCb.input);
-    pm.getExecutionManager()->startManaging(toolframeCb);
+    barrett::systems::Summer<jt_type, 3> customjtSum;
+    pm.getExecutionManager()->startManaging(customjtSum);
 
+    LeaderDynamics<DOF> leaderDynamics(pm.getExecutionManager());
     ExternalTorque<DOF> externalTorque(pm.getExecutionManager());
-    systems::connect(wam.gravity.output, externalTorque.wamGravityIn);
-    systems::connect(wam.jtSum.output, externalTorque.wamTorqueSumIn);
+    DynamicExternalTorque<DOF> dynamicExternalTorque(pm.getExecutionManager());
 
     barrett::systems::FirstOrderFilter<jt_type> extFilter;
     jt_type omega_p(180.0);
     extFilter.setLowPass(omega_p);
     pm.getExecutionManager()->startManaging(extFilter);
 
-    systems::connect(externalTorque.wamExternalTorqueOut, extFilter.input);
+    barrett::systems::FirstOrderFilter<jt_type> dynamicExtFilter;
+    dynamicExtFilter.setLowPass(omega_p);
+    pm.getExecutionManager()->startManaging(dynamicExtFilter);
 
+    // wire dynamics (same as before)
+    ja_type ja; ja.setConstant(0.0);
+    systems::Constant<ja_type> zeroAcceleration(ja);
+    pm.getExecutionManager()->startManaging(zeroAcceleration);
+
+    // Leader system (now wrist-enabled)
     Leader<DOF> leader(pm.getExecutionManager(), &hw, remoteHost, rec_port, send_port);
+
+    jt_type maxRate; maxRate << 50, 50, 50, 50;
+    systems::RateLimiter<jt_type> wamJPOutputRamp(maxRate, "ffRamp");
+
+    // optional prints
+    systems::PrintToStream<jt_type> printdynamicextTorque(pm.getExecutionManager(), "dynamicextTorque: ");
+    systems::PrintToStream<jt_type> printextTorque(pm.getExecutionManager(), "extTorque: ");
+    systems::PrintToStream<jt_type> printdynamicoutput(pm.getExecutionManager(), "dynamicoutput: ");
+    systems::PrintToStream<jt_type> printSC(pm.getExecutionManager(), "SC: ");
+
+    // filters for ja (as in your original)
+    double h_omega_p = 25.0;
+    barrett::systems::FirstOrderFilter<jv_type> hp1;
+    hp1.setHighPass(jv_type(h_omega_p), jv_type(h_omega_p));
+    systems::Gain<jv_type, double, ja_type> jaWAM(1.0);
+    pm.getExecutionManager()->startManaging(hp1);
+
+    barrett::systems::FirstOrderFilter<ja_type> jaFilter;
+    ja_type l_omega_p = ja_type::Constant(50.0);
+    jaFilter.setLowPass(l_omega_p);
+    pm.getExecutionManager()->startManaging(jaFilter);
+
+    systems::connect(wam.jvOutput, hp1.input);
+    systems::connect(hp1.output, jaWAM.input);
+    systems::connect(jaWAM.output, jaFilter.input);
+    // systems::connect(jaFilter.output, leaderDynamics.jaInputDynamics); // keep if you used it before
+
     systems::connect(wam.jpOutput, leader.wamJPIn);
     systems::connect(wam.jvOutput, leader.wamJVIn);
-    systems::connect(extFilter.output, leader.extTorqueIn);
+    systems::connect(wam.gravity.output, leader.wamGravIn);
+
+    systems::connect(wam.jpOutput, leaderDynamics.jpInputDynamics);
+    systems::connect(wam.jvOutput, leaderDynamics.jvInputDynamics);
+
+    systems::connect(leaderDynamics.dynamicsFeedFWD, leader.wamDynIn);
+
+    // ext torque path (choose source)
+    systems::connect(wam.gravity.output, externalTorque.wamGravityIn);
+    systems::connect(wam.jtSum.output,  externalTorque.wamTorqueSumIn);
+    systems::connect(externalTorque.wamExternalTorqueOut, extFilter.input);
+
+    // your dynamic ext torque path
+    systems::connect(customjtSum.output,              dynamicExternalTorque.wamTorqueSumIn);
+    systems::connect(leaderDynamics.dynamicsFeedFWD,  dynamicExternalTorque.wamDynamicsIn);
+    systems::connect(dynamicExternalTorque.wamExternalTorqueOut, dynamicExtFilter.input);
+    systems::connect(dynamicExternalTorque.wamExternalTorqueOut, leader.extTorqueIn);
+    // or: systems::connect(extFilter.output, leader.extTorqueIn);
+
+    // sum torque cmd + gravity + supervisory
+    systems::connect(leader.wamJPOutput,                 customjtSum.getInput(0));
+    systems::connect(wam.gravity.output,                 customjtSum.getInput(1));
+    systems::connect(wam.supervisoryController.output,   customjtSum.getInput(2));
+    connect(customjtSum.output, wam.input);
 
     wam.gravityCompensate();
 
-    std::string line;
-    v_type gainTmp;
-
+    std::string line; v_type gainTmp;
     bool going = true;
 
     while (going) {
         printf(">>> ");
         std::getline(std::cin, line);
+        if (line.empty()) { printf("\n"); continue; }
 
         switch (line[0]) {
-            case 'l':
-                if (leader.isLinked()) {
-                    leader.unlink();
-                } else {
-                    wam.moveTo(SYNC_POS);
-                    hw.moveTo({0, 0, 0});
+        case 'l':
+            if (leader.isLinked()) {
+                leader.unlink();
+            } else {
+                wam.moveTo(SYNC_POS, true);
+                hw.setTarget({0.0, 0.0, 0.0});  // no brace init
 
-                    printf("Press [Enter] to link with the other WAM.");
-                    waitForEnter();
-                    leader.tryLink();
-                    wam.trackReferenceSignal(leader.wamJPOutput);
+                printf("Press [Enter] to link with the other WAM.");
+                waitForEnter();
+                leader.tryLink();
+                wam.trackReferenceSignal(leader.theirJPOutput);
+                connect(leader.wamJPOutput, wam.input);
 
-                    btsleep(0.1); // wait an execution cycle or two
-                    if (leader.isLinked()) {
-                        printf("Linked with remote WAM.\n");
-                    } else {
-                        printf("WARNING: Linking was unsuccessful.\n");
-                    }
-                }
+                // connect(leader.wamJPOutput, wamJPOutputRamp.input);
+                // connect(wamJPOutputRamp.output, wam.input);
 
-                break;
+                btsleep(0.1);
+                if (leader.isLinked()) printf("Linked with remote WAM.\n");
+                else                    printf("WARNING: Linking was unsuccessful.\n");
+            }
+            break;
 
-            case 't':
-                size_t jointIndex;
-                {
-                    size_t jointNumber;
-                    std::cout << "\tJoint: ";
-                    std::cin >> jointNumber;
-                    jointIndex = jointNumber - 1;
+        case 't': {
+            size_t jointNumber;
+            std::cout << "\tJoint: ";
+            std::cin >> jointNumber;
+            size_t jointIndex = jointNumber - 1;
+            if (jointIndex >= DOF) { std::cout << "\tBad joint number: " << jointNumber; break; }
 
-                    if (jointIndex >= DOF) {
-                        std::cout << "\tBad joint number: " << jointNumber;
-                        break;
-                    }
-                }
+            char gainId;
+            std::cout << "\tGain identifier (p, i, or d): ";
+            std::cin >> line; gainId = line[0];
 
-                char gainId;
-                std::cout << "\tGain identifier (p, i, or d): ";
-                std::cin >> line;
-                gainId = line[0];
+            std::cout << "\tCurrent value: ";
+            switch (gainId) {
+            case 'p': gainTmp = wam.jpController.getKp(); break;
+            case 'i': gainTmp = wam.jpController.getKi(); break;
+            case 'd': gainTmp = wam.jpController.getKd(); break;
+            default:  std::cout << "\tBad gain identifier.";
+            }
+            std::cout << gainTmp[jointIndex] << std::endl;
 
-                std::cout << "\tCurrent value: ";
-                switch (gainId) {
-                    case 'p':
-                        gainTmp = wam.jpController.getKp();
-                        break;
-                    case 'i':
-                        gainTmp = wam.jpController.getKi();
-                        break;
-                    case 'd':
-                        gainTmp = wam.jpController.getKd();
-                        break;
+            std::cout << "\tNew value: ";
+            std::cin >> gainTmp[jointIndex];
+            switch (gainId) {
+            case 'p': wam.jpController.setKp(gainTmp); break;
+            case 'i': wam.jpController.setKi(gainTmp); break;
+            case 'd': wam.jpController.setKd(gainTmp); break;
+            default:  std::cout << "\tBad gain identifier.";
+            }
+        } break;
 
-                    default:
-                        std::cout << "\tBad gain identifier.";
-                }
-                std::cout << gainTmp[jointIndex] << std::endl;
+        case 'x':
+            going = false; break;
 
-                std::cout << "\tNew value: ";
-                std::cin >> gainTmp[jointIndex];
-                switch (gainId) {
-                    case 'p':
-                        wam.jpController.setKp(gainTmp);
-                        break;
-                    case 'i':
-                        wam.jpController.setKi(gainTmp);
-                        break;
-                    case 'd':
-                        wam.jpController.setKd(gainTmp);
-                        break;
-
-                    default:
-                        std::cout << "\tBad gain identifier.";
-                }
-
-                break;
-            case 'x':
-                going = false;
-                break;
-
-            default:
-                printf("\n");
-                printf("    'l' to toggle linking with other WAM\n");
-                printf("    't' to tune control gains\n");
-                printf("    'x' to exit\n");
-
-                break;
+        default:
+            printf("\n");
+            printf("    'l' to toggle linking with other WAM (and wrist)\n");
+            printf("    't' to tune control gains\n");
+            printf("    'x' to exit\n");
+            break;
         }
     }
 
     pm.getSafetyModule()->waitForMode(SafetyModule::IDLE);
     hw.stop();
-
     return 0;
 }
+
--- src/leader.h
+++ src/leader.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <haptic_wrist/haptic_wrist.h>
-
 #include <boost/asio.hpp>
-
 #include "udp_handler.h"
 #include <barrett/detail/ca_macro.h>
-#include <barrett/systems.h>
+#include <barrett/systems/abstract/single_io.h>
 #include <barrett/thread/abstract/mutex.h>
 #include <barrett/units.h>
 
@@ -14,15 +12,24 @@ class Leader : public barrett::systems::System {
     BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
 
   public:
+    // Inputs from local WAM
     Input<jp_type> wamJPIn;
     Input<jv_type> wamJVIn;
-    Input<jt_type> extTorqueIn;
-    Output<jt_type> wamJPOutput;
+    Input<jt_type> extTorqueIn;   // may be undefined
+    Input<jt_type> wamGravIn;     // τ_g
+    Input<jt_type> wamDynIn;      // τ_dyn (feedforward)
+
+    // Outputs
+    Output<jt_type> wamJPOutput;     // control torque to local WAM
+    Output<jp_type> theirJPOutput;   // peer arm JP (for tracking/logging)
 
     enum class State { INIT, LINKED, UNLINKED };
 
-    explicit Leader(barrett::systems::ExecutionManager* em, haptic_wrist::HapticWrist* hw,
-                    const std::string& remoteHost, int rec_port = 5554, int send_port = 5555,
+    explicit Leader(barrett::systems::ExecutionManager* em,
+                    haptic_wrist::HapticWrist* hw,             // may be nullptr if no wrist device
+                    const std::string& remoteHost,
+                    int rec_port = 5555,                       // leader receives follower's 5555
+                    int send_port = 5554,                      // leader sends on 5554
                     const std::string& sysName = "Leader")
         : System(sysName)
         , theirJp(0.0)
@@ -32,40 +39,49 @@ class Leader : public barrett::systems::System {
         , wamJPIn(this)
         , wamJVIn(this)
         , extTorqueIn(this)
+        , wamGravIn(this)
+        , wamDynIn(this)
         , wamJPOutput(this, &jtOutputValue)
-        , udp_handler(remoteHost, send_port, rec_port)
+        , theirJPOutput(this, &theirJPOutputValue)
+        , udp_handler(remoteHost, send_port, rec_port)  // (tx, rx)
         , hw(hw)
-        , state(State::INIT) {
-
-        kp << 600, 500, 250, 120;
-        kd << 30, 20, 15, 5;
+        , state(State::INIT)
+    {
+        // keep your shapes
+        kp << 750, 1000, 400, 200;
+        kd << 8.3, 8, 3.3, 0.8;
+        cf << 0.375, 0.4, 0.2, 0.1;
 
         if (em != NULL) {
             em->startManaging(*this);
         }
     }
 
-    virtual ~Leader() {
-        this->mandatoryCleanUp();
-    }
+    virtual ~Leader() { this->mandatoryCleanUp(); }
+    virtual bool inputsValid() { return true; }
 
-    bool isLinked() const {
-        return state == State::LINKED;
-    }
-    void tryLink() {
-        BARRETT_SCOPED_LOCK(this->getEmMutex());
-        state = State::LINKED;
-    }
-    void unlink() {
-        BARRETT_SCOPED_LOCK(this->getEmMutex());
-        state = State::UNLINKED;
-    }
+    bool isLinked() const { return state == State::LINKED; }
+    void tryLink()        { BARRETT_SCOPED_LOCK(this->getEmMutex()); state = State::LINKED; }
+    void unlink()         { BARRETT_SCOPED_LOCK(this->getEmMutex()); state = State::UNLINKED; }
 
   protected:
+    // Output value holders
     typename Output<jt_type>::Value* jtOutputValue;
+    typename Output<jp_type>::Value* theirJPOutputValue;
+
+    // Local arm state
     jp_type wamJP;
     jv_type wamJV;
     jt_type extTorque;
+    jt_type wamGrav;
+    jt_type wamDyn;
+
+    // Wrist vectors (3-DOF)
+    haptic_wrist::jp_type wristJP;      // local wrist pos
+    haptic_wrist::jv_type wristJV;      // local wrist vel
+    haptic_wrist::jp_type theirWristJp; // received wrist pos
+
+    // Network payloads: DOF + 3
     Eigen::Matrix<double, DOF + 3, 1> sendJpMsg;
     Eigen::Matrix<double, DOF + 3, 1> sendJvMsg;
     Eigen::Matrix<double, DOF + 3, 1> sendExtTorqueMsg;
@@ -73,36 +89,54 @@ class Leader : public barrett::systems::System {
     using ReceivedData = typename UDPHandler<DOF + 3>::ReceivedData;
 
     virtual void operate() {
-
-        // TODO: change back to 1.5, likely need to scale vel as well
+        // optional joint scaling for wrist axes
         double j5_scale = 1.0;
         double j7_scale = 1.0;
 
-        wamJP = wamJPIn.getValue();
-        wamJV = wamJVIn.getValue();
-        extTorque = extTorqueIn.getValue();
+        // Read WAM inputs
+        wamJP  = wamJPIn.getValue();
+        wamJV  = wamJVIn.getValue();
+        wamGrav = wamGravIn.getValue();
+        wamDyn  = wamDynIn.getValue();
 
+        if (extTorqueIn.valueDefined()) {
+            extTorque = extTorqueIn.getValue();
+        } else {
+            extTorque.setZero();
+        }
+
+        // Read wrist device (if available)
         haptic_wrist::jp_type wristJP = hw->getPosition();
         haptic_wrist::jp_type wristJV = hw->getVelocity();
+
+        // Pack outgoing messages (arm + wrist)
         sendJpMsg << wamJP, wristJP;
         sendJvMsg << wamJV, wristJV;
-        sendExtTorqueMsg << extTorque, 0, 0, 0;
+
+        // Only arm external torque is meaningful here; pad wrist torques with zeros
+        sendExtTorqueMsg << extTorque, 0.0, 0.0, 0.0;
+
+        // Example scaling of J5 and J7 (index 4 and 6 in the concatenated vector)
         sendJpMsg(4) = j5_scale * sendJpMsg(4);
         sendJpMsg(6) = j7_scale * sendJpMsg(6);
 
         udp_handler.send(sendJpMsg, sendJvMsg, sendExtTorqueMsg);
 
+        // receive peer
         boost::optional<ReceivedData> received_data = udp_handler.getLatestReceived();
         auto now = std::chrono::steady_clock::now();
         if (received_data && (now - received_data->timestamp <= TIMEOUT_DURATION)) {
-
-            theirJp = received_data->jp.template head<DOF>();
-            theirWristJp = received_data->jp.template tail<3>();
-            theirWristJp(0) = theirWristJp(0) / j5_scale;
-            theirWristJp(2) = theirWristJp(2) / j7_scale;
-
-            theirJv = received_data->jv.template head<DOF>();
+            theirJp        = received_data->jp.template head<DOF>();
+            theirWristJp   = received_data->jp.template tail<3>();
+            theirJv        = received_data->jv.template head<DOF>();
             theirExtTorque = received_data->extTorque.template head<DOF>();
+
+            // undo scaling
+            theirWristJp(0) /= j5_scale;
+            theirWristJp(2) /= j7_scale;
+
+            theirJPOutputValue->setData(&theirJp);
+
         } else {
             if (state == State::LINKED) {
                 std::cout << "lost link" << std::endl;
@@ -110,49 +144,62 @@ class Leader : public barrett::systems::System {
             }
         }
 
+        // state machine
         switch (state) {
             case State::INIT:
-                // Used so haptic wirst holds on moveTo command
+                hw->setTarget(wristJP); // hold
                 control.setZero();
                 jtOutputValue->setData(&control);
                 break;
+
             case State::LINKED:
-                // Active teleop. Only the callee can transition to LINKED
-                hw->setPosition(theirWristJp);
-                control = compute_control(theirJp, theirJv, theirExtTorque, wamJP, wamJV);
+                hw->setTarget(theirWristJp);
+                control = compute_control(
+                    theirJp, theirJv, theirExtTorque,
+                    wamJP,   wamJV,   extTorque,
+                    wamGrav, wamDyn
+                );
                 jtOutputValue->setData(&control);
                 break;
+
             case State::UNLINKED:
-                // Changed to unlinked with either timeout or callee.
-                hw->setPosition(wristJP);
+                hw->setTarget(wristJP);
                 control.setZero();
                 jtOutputValue->setData(&control);
                 break;
         }
     }
 
+    // peer arm state & control
     jp_type theirJp;
-    jp_type theirJv;
+    jv_type theirJv;
     jt_type theirExtTorque;
     jt_type control;
-    haptic_wrist::jp_type theirWristJp;
 
   private:
     DISALLOW_COPY_AND_ASSIGN(Leader);
-    haptic_wrist::HapticWrist* hw;
+    haptic_wrist::HapticWrist* hw;     // may be nullptr
     std::mutex state_mutex;
-    jp_type joint_positions;
     UDPHandler<DOF + 3> udp_handler;
     const std::chrono::milliseconds TIMEOUT_DURATION = std::chrono::milliseconds(20);
     State state;
-    Eigen::Vector4d kp;
-    Eigen::Vector4d kd;
 
-    jt_type compute_control(const jp_type& ref_pos, const jv_type& ref_vel, const jt_type& feedforward,
-                            const jp_type& cur_pos, const jv_type& cur_vel) {
-        jt_type pos_term = kp.asDiagonal() * (ref_pos - cur_pos);
-        jt_type vel_term = kd.asDiagonal() * (ref_vel - cur_vel);
-        jt_type feedforward_term = 0.4 * feedforward;
-        return pos_term + vel_term - feedforward_term;
-    };
+    Eigen::Matrix<double, DOF, 1> kp;
+    Eigen::Matrix<double, DOF, 1> kd;
+    Eigen::Matrix<double, DOF, 1> cf;
+
+    // your menu (default u4: Lawrence + dyn comp)
+    jt_type compute_control(const jp_type& ref_pos, const jv_type& ref_vel, const jt_type& ref_extTorque,
+                            const jp_type& cur_pos, const jv_type& cur_vel, const jt_type& cur_extTorque,
+                            const jt_type& cur_grav, const jt_type& cur_dyn) {
+        jt_type u1 = 0.0 * cur_extTorque;
+        jt_type u2 = cur_dyn - cur_grav;
+        jt_type u3 = -0.5 * ref_extTorque;
+        jt_type u4 = -0.5 * ref_extTorque + cur_dyn - cur_grav; // Lawrence ideal
+        jt_type u5 = -0.5 * ref_extTorque - 0.15 * (ref_extTorque + cur_extTorque);
+        jt_type u6 = -0.5 * ref_extTorque - 0.15 * (ref_extTorque + cur_extTorque) + cur_dyn - cur_grav;
+        jt_type u7 = -0.5 * cur_extTorque;
+        jt_type u8 = -0.25 * (ref_extTorque + cur_extTorque);
+        return u4;
+    }
 };
